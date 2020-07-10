6eca4c4dabb9b8f7901fb778526a6271354f6d01
Anurag Mishra <mishranurag@gmail.com>
2020-07-10 13:24:44 -0700

    Added files for HSPF and EFDC+ model

---
+++ EFDC_HSPF_Files/DM-14_Lake_T_HYD-WQ_Model/Models/Hyd/0run_efdc.bat
@@ -0,0 +1,8 @@
+SET PATH=C:\Program Files\DSI\EEMS10.2;%PATH%
+SET KMP_AFFINITY=granularity=fine,compact,1,0
+
+TITLE C:\BASINS45\modelout\EFDC_HSPF\DM-14_Lake_T_HYD-WQ_Model\Models\Hyd
+C:
+CD "C:\BASINS45\modelout\EFDC_HSPF\DM-14_Lake_T_HYD-WQ_Model\Models\Hyd"
+"C:\Program Files\DSI\EEMS10.2\EFDCPlus_MPI_10.2_200622_SPx64.exe"  -NT3
+
